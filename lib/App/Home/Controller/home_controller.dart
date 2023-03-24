2dbb0cbb4464604f9d1aacff171db1e9684c30b7
Amal pk <amalashok7902@gmail.com>
2023-03-24 12:32:44 +0530

    Home Page Icons

---
+++ lib/App/Home/Controller/home_controller.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class HomePageController extends ChangeNotifier {
+  List<Color> clr = [
+    Colors.red,
+    Colors.green,
+    Colors.orange,
+    Colors.yellow,
+    Colors.pink,
+    Colors.purple,
+    Colors.amber,
+    Colors.blue,
+    Colors.grey,
+    Colors.teal,
+  ];
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:liveombre/App/Bottom%20Navigation/Controller/bottom_navigation_controller.dart';
 import 'package:liveombre/App/Bottom%20Navigation/View/bottom_navigation.dart';
+import 'package:liveombre/App/Home/Controller/home_controller.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -16,6 +17,9 @@ class MyApp extends StatelessWidget {
       providers: [
         ChangeNotifierProvider(
           create: (context) => BottomNavigationController(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => HomePageController(),
         ),
       ],
       child: MaterialApp(
